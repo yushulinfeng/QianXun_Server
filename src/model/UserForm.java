f58a4141179d77ebe8b2b3728da10241b14f6b60
LinFeng <china2012syl@163.com>
2017-05-05 10:30:55 +0800

    服务端-Struts引入与配置

---
+++ src/model/UserForm.java
@@ -0,0 +1,43 @@
+package model;
+
+/**
+ * Created by YuShuLinFeng on 2017/4/28.
+ */
+public class UserForm {
+    private String userid;
+    private String username;
+    private String userpass;
+    private String usernick;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpass() {
+        return userpass;
+    }
+
+    public void setUserpass(String userpass) {
+        this.userpass = userpass;
+    }
+
+    public String getUsernick() {
+        return usernick;
+    }
+
+    public void setUsernick(String usernick) {
+        this.usernick = usernick;
+    }
+}
